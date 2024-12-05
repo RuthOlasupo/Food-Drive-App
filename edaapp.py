54d8e549c2138c141a04d541ed12339ae99be0ee
Ruth Olasupo <ruthluvolas@gmail.com>
2024-12-04 17:26:51 -0700

    Update edaapp.py

--- edaapp.py
+++ edaapp.py
@@ -61,21 +61,19 @@ def dashboard():
 
 # Page 2: Exploratory Data Analysis (EDA)
 
-import streamlit.components.v1 as components
-
 def exploratory_data_analysis():
     # Set the page title
     st.title("Visualizations")
-
     # Embed Tableau visualization using an HTML iframe
-    tableau_url = "https://public.tableau.com/views/EDA-Demo/NoOutliersDashboard?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
-    
-    iframe_code = f"""
-    <iframe src="{tableau_url}" width="800" height="600" style="border: none;"></iframe>
-    """
-    
-    # Use components to embed the iframe
-    components.html(iframe_code, height=600)
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <iframe src="<div class='tableauPlaceholder' id='viz1733358354835' style='position: relative'><noscript><a href='#'><img alt='Edmonton Food Drive - Time Series Analysis (2023 - 2024) ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ED&#47;EDA-Demo&#47;NoOutliersDashboard&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='EDA-Demo&#47;NoOutliersDashboard' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ED&#47;EDA-Demo&#47;NoOutliersDashboard&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1733358354835');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='420px';vizElement.style.maxWidth='1350px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='420px';vizElement.style.maxWidth='1350px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1677px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"
+            width="800" height="600" style="border: none;"></iframe>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 
